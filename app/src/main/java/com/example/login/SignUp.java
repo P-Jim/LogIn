196d017d801d0aca0d6e5c623cccfd2ab151c80d
Jun Yu (Jimmy) Ma <Jimmy.ma3615@gmail.com>
2020-03-19 05:01:12 -0500

    Progress with Sign Up

--- app/src/main/java/com/example/login/SignUp.java
+++ app/src/main/java/com/example/login/SignUp.java
@@ -2,7 +2,11 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void clickFunction (View view){
+        EditText myText = (EditText) findViewById(R.id.editText);
+        String str = myText.getText().toString();
+        boolean Validity = str.matches("^.*[^a-zA-Z ]{2,20}.*$");
+        if (Validity){
+            goToNext(str);
+        }
+        else {
+            Toast.makeText(SignUp.this, "Invalid input, please only input 20 valid characters", Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
+
+    public void goToNext (String s){
+        Intent intent = new Intent(this, SignUp2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 }
