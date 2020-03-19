38c64fa0f572d014a58b817ed792139cf2577b02
Jun Yu (Jimmy) Ma <Jimmy.ma3615@gmail.com>
2020-03-18 23:32:52 -0500

    Main Activity Complete

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void clickFunction (View view){
+        goToActivityLogin();
+    }
+    public void clickFunction2 (View view){
+        goToActivitySignUp();
+    }
+    public void goToActivityLogin (){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+    public void goToActivitySignUp (){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
