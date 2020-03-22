2314fcf27c4a4bcdaece3ac5a3ffd260b466be34
Jun Yu (Jimmy) Ma <Jimmy.ma3615@gmail.com>
2020-03-21 22:04:46 -0500

    Issues getting OnPreferenceChangeListener to work

--- app/src/main/java/com/example/login/AccountPage.java
+++ app/src/main/java/com/example/login/AccountPage.java
@@ -3,8 +3,11 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.TextView;
 
 public class AccountPage extends AppCompatActivity {
 
@@ -25,6 +28,26 @@ public class AccountPage extends AppCompatActivity {
     public void clickFunction2 (View view){
 
     }
-
-
+    TextView NameText;
+    TextView PhoneText;
+    TextView DescriptionText;
+    TextView Location;
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean privacy = prefs.getBoolean("switch_preference_1", true);
+        String name = prefs.getString("edit_text_preference_4",  "");
+        String phone = prefs.getString("edit_text_preference_3",  "");
+        String description = prefs.getString("edit_text_preference_5",  "");
+        String location = prefs.getString("edit_text_preference_6",  "");
+        NameText = (TextView) findViewById(R.id.profile_name);
+        PhoneText = (TextView) findViewById(R.id.phone);
+        DescriptionText = (TextView) findViewById(R.id.info);
+        Location = (TextView) findViewById(R.id.location);
+        NameText.setText(name);
+        PhoneText.setText(phone);
+        DescriptionText.setText(description);
+        Location.setText(location);
+    }
 }
